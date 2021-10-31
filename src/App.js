be0a64d3ba6e75f26ca983fdf86efebbc3990464
MRSA-ID <sanggisda@gmail.com>
2021-10-31 21:42:04 +0700

    add products/store/rate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,6 +8,8 @@ import {
   useHistory,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProductsListing from "./components/Products/ProductsListing";
+import ProductDetails from "./components/Products/ProductDetails";
 /* list component yang belum :
 1.products details
 2.home
@@ -15,12 +17,14 @@ import Navbar from "./components/Navbar";
 4.chart
 5.admin page
 */
-
 function App() {
   return (
     <Router>
       <div className="w-full h-screen">
         <Navbar />
+        <Route exact path="/" component={ProductsListing} />
+        <Route path="/product/:productId" component={ProductDetails} />
+        <Redirect from="*" to="/" />
       </div>
     </Router>
   );
---
+++ src/components/Products/ProductsComponent.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+
+const ProductsComponent = () => {
+  const products = useSelector((state) => state.products);
+  const history = useHistory();
+  const renderList = products.map((product) => {
+    const { id, title, price, image, category } = product;
+    return (
+      <div className="group relative pb-14" key={id}>
+        <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img
+            key={id}
+            src={image}
+            alt={title}
+            className="w-full h-full object-top object-cover lg:w-full lg:h-full"
+          />
+        </div>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <p>
+                <span aria-hidden="true" className="absolute inset-0"></span>
+                {title}
+              </p>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{category}</p>
+          </div>
+          <p className="text-sm font-medium text-gray-900">{price}</p>
+        </div>
+        <div className=" flex justify-between absolute bottom-0 w-full">
+          <button
+            className="bg-blue-400 text-white px-3 py-2 mt-2 text-lg rounded-md font-medium"
+            onClick={() => history.push(`/product/${id}`)}>
+            Details
+          </button>
+          <button className="font-bold px-3 py-2 mt-2 bg-green-500 text-white text-lg rounded-md">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    );
+  });
+  return <>{renderList}</>;
+};
+
+export default ProductsComponent;
